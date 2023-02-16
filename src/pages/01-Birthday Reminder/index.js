0b1ef6b2112fe6f04714398d28376fd48b30fdef
prasetyoow <praztamer@gmail.com>
2023-02-16 16:49:57 +0700

    fixed style for Birthday project

--- src/pages/01-Birthday Reminder/index.js
+++ src/pages/01-Birthday Reminder/index.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
-import "./birthday.css";
 
 function App() {
   const [people, setPeople] = useState(data);
   return (
-    <main>
-      <section className="container">
-        <h3>{people.length} Birthdays today</h3>
-        <List people={people} />
-        <button onClick={() => setPeople([])}>clear all</button>
-      </section>
-    </main>
+    <>
+      <main className="main__birthday">
+        <section className="container__birthday">
+          <h3>{people.length} Birthdays today</h3>
+          <List people={people} />
+          <button onClick={() => setPeople([])}>clear all</button>
+        </section>
+      </main>
+    </>
   );
 }
 
