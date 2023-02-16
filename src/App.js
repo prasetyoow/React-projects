ba7bc0be62924a2f183b35fe4838b8feabfb5c7d
prasetyoow <praztamer@gmail.com>
2023-02-16 16:49:14 +0700

    added new route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/00-Home";
 import BirthdayReminder from "./pages/01-Birthday Reminder";
 import Tours from "./pages/02-Tours";
+import Reviews from "./pages/03-Reviews";
+import Accordion from "./pages/04-Accordion";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/birthday" element={<BirthdayReminder />} />
         <Route path="/tours" element={<Tours />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/accordion" element={<Accordion />} />
       </Routes>
     </>
   );
