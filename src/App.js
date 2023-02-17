0c0eb85770a4b933dc4553d2c11f316e64e8ad9a
prasetyoow <praztamer@gmail.com>
2023-02-17 15:31:59 +0700

    added more routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import BirthdayReminder from "./pages/01-Birthday Reminder";
 import Tours from "./pages/02-Tours";
 import Reviews from "./pages/03-Reviews";
 import Accordion from "./pages/04-Accordion";
+import Menu from "./pages/05-Menu";
+import Tabs from "./pages/06-Tabs";
+import Slider from "./pages/07-Slider";
+import LoremIpsum from "./pages/08-Lorem-Ipsum";
+import ColorGenerator from "./pages/09-Color-Generator";
 
 function App() {
   return (
@@ -15,6 +20,11 @@ function App() {
         <Route path="/tours" element={<Tours />} />
         <Route path="/reviews" element={<Reviews />} />
         <Route path="/accordion" element={<Accordion />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/tabs" element={<Tabs />} />
+        <Route path="/slider" element={<Slider />} />
+        <Route path="/loremipsum" element={<LoremIpsum />} />
+        <Route path="/colorgenerator" element={<ColorGenerator />} />
       </Routes>
     </>
   );
